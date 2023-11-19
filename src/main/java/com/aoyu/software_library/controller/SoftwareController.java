88455ea20cea4f63a81b047c2431d3ab54a8112a
aoyu <aoyu3399@gmail.com>
2023-11-19 14:44:53 +0800

    登录jwt

---
+++ src/main/java/com/aoyu/software_library/controller/SoftwareController.java
@@ -0,0 +1,20 @@
+package com.aoyu.software_library.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @BelongsProject: software_library
+ * @BelongsPackage: com.aoyu.software_library.controller
+ * @Author: AoYu
+ * @CreateTime: 2023-11-19  14:43
+ * @Description: TODO
+ * @Version: 1.0
+ */
+
+@RestController
+public class SoftwareController {
+
+
+
+
+}
